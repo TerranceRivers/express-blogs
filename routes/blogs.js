2ff41ca18590c82180e84fe1d270f18dcc36fa73
TerranceRivers <109315955+TerranceRivers@users.noreply.github.com>
2023-05-24 15:53:09 -0400

    update2

--- routes/blogs.js
+++ routes/blogs.js
@@ -2,11 +2,6 @@ var express = require("express");
 var router = express.Router();
 const { v4: uuid } = require("uuid");
 
-/* GET home page. */
-// router.get("/", function (req, res, next) {
-//   res.render("index", { title: "Express" });
-// });
-
 router.get("/", (req, res) => {
   res.json({ message: "hello from blog" });
 });
